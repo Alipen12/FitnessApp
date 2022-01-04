abaad59102b359000dc38d440d501afec3aee5ae
Ilia <Ilia@DESKTOP-APRIFVI>
2022-01-04 20:50:28 +0700

    Добавьте файлы проекта.

---
+++ FitnessApp.BL/Model/Gender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.BL.Model
+{
+    /// <summary>
+    /// Пол
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Сздать новый пол
+        /// </summary>
+        /// <param name="name"> Имя пола </param>
+        /// <exception cref="ArgumentNullException"></exception>
+
+        public Gender(string name)
+        {
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null" , nameof(name));
+            }
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
